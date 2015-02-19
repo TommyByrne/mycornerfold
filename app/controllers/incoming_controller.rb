94587b06d032e75b9dd58825fc47e979ce42ca44
tommybyrne <tbyrn687@gmail.com>
2015-02-18 21:59:24 -0600

    Changed incoming create method

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,31 +4,21 @@ class IncomingController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:create]
 
 
-  # def create
-  #   puts "INCOMING PARAMS HERE: #{params}"
-
-  #   @user = User.where(email: params["envelope"]["sender"]).take
-  #   @topic = Topic.where(email: params["message"]["headers"]["subject"]).take
-
-  #   unless @topic
-  #     topic = Topic.new(title: params["message"]["headers"]["subject"])
-  #     topic.save!
-  #   end
-
-  #   @bookmark = Bookmark.new(url: params["body-plain"], topic: @topic)
-  #   bookmark.save!
-  #   head 200
-  # rescue => e
-  #   puts "failed because #{e}"
-
-  # end
   def create
-     puts "INCOMING PARAMS HERE: #{params}"
-    @user = User.where(email: params['sender']).first
-    @topic = Topic.where(name: params[:subject]).first_or_create
-    unless @user.name == "User"
-      @topic.bookmarks.create(link: params['stripped-text'], user_id: @user.id)
+    puts "INCOMING PARAMS HERE: #{params}"
+
+    @user = User.where(email: params['sender']).take
+    @topic = Topic.where(title: params['subject']).take
+
+    unless @topic
+      topic = Topic.new(title: params['subject'])
+      topic.save!
     end
+
+    @bookmark = Bookmark.new(url: params["stripped-text"], user: @user, topic: @topic)
+    bookmark.save!
     head 200
+  rescue => e
+    puts "failed because #{e}"
   end
 end
