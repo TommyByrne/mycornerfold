e6d7df6888adfb1b52067ea25573770c099c14c7
tommybyrne <tbyrn687@gmail.com>
2015-02-25 22:41:09 -0600

    Sorted User Show page by topics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @bookmarks = @user.bookmarks.group_by{ |b| b.topic}
     @likes = current_user.likes
+    @topics = Topic.all
+
 
    end
 end
